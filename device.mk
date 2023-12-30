3ae304cac13864c9fc0f39c9b113519487fe4588
bengris32 <bengris32@protonmail.ch>
2023-12-30 21:01:27 +0000

    ruby: Include MiuiCamera product if exists
    Change-Id: I02155e538a24d035bf0a621a244f0dedc581b81c

--- device.mk
+++ device.mk
@@ -341,4 +341,5 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
 # Inherit the proprietary files
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-ruby/device.mk)
 $(call inherit-product, vendor/xiaomi/ruby/ruby-vendor.mk)
