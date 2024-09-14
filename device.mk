87c098bcec15286023235a9b76cb11e7a65b5b85
bengris32 <bengris32@protonmail.ch>
2024-09-14 14:50:24 +0100

    ruby: Kill CarrierConfigOverlay
    * Only seems to cause problems, and we don't really need it as
      we have force enabled IMS features again in commit e5018c6.
    
    Change-Id: Id8ffe30aee1d57ee4b4d61828b605f9da33f1d8c
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -161,7 +161,6 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
-    CarrierConfigOverlayRuby \
     FrameworksResOverlayRuby \
     FrameworksResOverlayRubyPlus \
     FrameworksResOverlayRubyProPlus \
