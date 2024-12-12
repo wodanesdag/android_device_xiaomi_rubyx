4905c3b9be89cc60c8b34ff66543a0b2d90904fc
wodanesdag <wodanesdag@proton.me>
2024-12-12 12:43:33 +0700

    ruby: Update blobs from V816.0.12.0.UMOMIXM

--- lineage_ruby.mk
+++ lineage_ruby.mk
@@ -25,6 +25,6 @@ PRODUCT_SYSTEM_DEVICE := ruby
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="ruby-user 14 UP1A.231005.007 V816.0.4.0.UMOMIXM release-keys"
+    BuildDesc="ruby-user 14 UP1A.230620.001 V816.0.12.0.UMOMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/ruby_global/ruby:14/UP1A.231005.007/V816.0.4.0.UMOMIXM:user/release-keys
+    BuildFingerprint=Redmi/ruby_global/ruby:14/UP1A.230620.001/V816.0.12.0.UMOMIXM:user/release-keys
