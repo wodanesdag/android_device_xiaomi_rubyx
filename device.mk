2db371283bb9dc305685ec480eac72fb918b880d
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    ruby: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- device.mk
+++ device.mk
@@ -135,7 +135,7 @@ PRODUCT_COPY_FILES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # Lineage Health
 PRODUCT_PACKAGES += \
