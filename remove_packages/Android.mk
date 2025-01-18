10c071474fb811c2baf3ef5ae073c595dc8b584f
wodanesdag <wodanesdag@proton.me>
2025-01-18 18:24:02 +0700

    ruby: Remove more packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Etar FMRadio Glimpse Jelly MatLog Recorder Seedvault Stk Twelve Updater
+LOCAL_OVERRIDES_PACKAGES := Camelot Etar FMRadio Gallery2 Glimpse HTMLViewer Jelly MatLog MusicFX Recorder Seedvault Stk Twelve Updater
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
